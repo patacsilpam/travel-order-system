7a91bea6ca9eefc59af75dcee2fbc93cfcd457fd
patacsilpam <pamelampatacsil@gmail.com>
2024-01-11 00:49:09 +0800

    feat:working on sidebar design

--- resources/views/superadmin/dashboard.blade.php
+++ resources/views/superadmin/dashboard.blade.php
@@ -9,10 +9,25 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Dashboard</title>
 </head>
+<style>
+    li{
+        list-style: none;
+    }
+    ul>li:hover{
+        background: #bcbcbc5c;
+    }
+    .nav-link{
+        color: #B2B2B2;
+        font-size: 1.2rem;
+    }
+    .nav-link:hover{
+        color: #ffffff;
+    }
+</style>
 <body>
     <div class="">
         <!--Sidebar-->
-            <div class="col h-full p-2" style="height: 100vh; width: 350px; background-color:#2B2D37;">
+            <div class="col h-full" style="height: 100vh; width: 350px; background-color:#2B2D37; position: fixed;">
                 <div  class="d-flex align-items-center">
                     <img src="/images/logo.png" alt="NCIP LOGO" class="p-2 h-50">
                     <div class="d-flex flex-column  text-white">
@@ -20,17 +35,31 @@
                         <span>Employee Travel Order System</span>
                     </div>
                 </div>
-                <nav style="margin-top: 100px">
-                    <ul class="list-group" style="line-height: 50px">
-                        <li><a href="#" class="text-decoration-none" style="color:#B2B2B2"><i class='bx bxs-dashboard'></i> Dashboard</a></li>
-                        <li><a href="#" class="text-decoration-none" style="color:#B2B2B2"><i class='bx bxs-file-blank'></i> Request</a></li>
-                        <li><a href="#" class="text-decoration-none" style="color:#B2B2B2"><i class='bx bxs-map' ></i> Travel Orders</a></li>
-                        <li><a href="#" class="text-decoration-none" style="color:#B2B2B2"><i class='bx bxs-user'></i> Employees</a></li>
-                        <li><a href="#" class="text-decoration-none" style="color:#B2B2B2"><i class='bx bxs-user'></i> Officials</a></li>
-                        <li><a href="#" class="text-decoration-none" style="color:#B2B2B2"><i class='bx bx-building' ></i> Divisions</a></li>
-                        <li><a href="#" class="text-decoration-none" style="color:#B2B2B2"><i class='bx bxs-user'></i> Users</a></li>
-                        <li><a href="#" class="text-decoration-none" style="color:#B2B2B2"><i class='bx bxs-cog' ></i> Settings</a></li>
+                <nav style="margin-top: 70px">
+                    <ul class="list-group">
+                        <li class="p-3"><a href="#" class="text-decoration-none nav-link" ><i class='bx bxs-dashboard'></i> Dashboard</a></li>
+                        <li class="p-3"><a href="#" class="text-decoration-none nav-link"><i class='bx bxs-file-blank'></i> Request</a></li>
+                        <li class="p-3"><a href="#" class="text-decoration-none nav-link"><i class='bx bxs-map' ></i> Travel Orders</a></li>
+                        <li class="p-3"><a href="#" class="text-decoration-none nav-link"><i class='bx bxs-user'></i> Employees</a></li>
+                        <li class="p-3"><a href="#" class="text-decoration-none nav-link"><i class='bx bxs-user'></i> Officials</a></li>
+                        <li class="p-3"><a href="#" class="text-decoration-none nav-link"><i class='bx bx-building' ></i> Divisions</a></li>
+                        <li class="p-3"><a href="#" class="text-decoration-none nav-link"><i class='bx bxs-user'></i> Users</a></li>
+                        <li class="p-3">
+                            <a class=" text-decoration-none nav-link" data-bs-toggle="collapse" href="#settingsCollapse" role="button" aria-expanded="false" aria-controls="settingsCollapse">
+                                <i class='bx bxs-cog' ></i> Settings <i class='bx bxs-down-arrow' style="margin-left: 170px;"></i>
+                            </a>
+                            <div class="collapse multi-collapse" id="settingsCollapse">
+                                <ul class="list-group mt-2">
+                                    <li class="p-2"><a href="#" class="text-decoration-none text-white">Station</a></li>
+                                    <li class="p-2"><a href="#" class="text-decoration-none text-white">Position</a></li>
+                                    <li class="p-2"><a href="#" class="text-decoration-none text-white">Role</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        
                     </ul>
+                    
+                    
                 </nav>
             </div>
     </div>
