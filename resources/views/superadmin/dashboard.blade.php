a55448f7e74ce08a5eea982d2c967574a8895455
patacsilpam <pamelampatacsil@gmail.com>
2024-01-10 23:14:12 +0800

    travel order system

---
+++ resources/views/superadmin/dashboard.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dashboard</title>
+</head>
+<body>
+    <div class="">
+        <!--Sidebar-->
+            <div class="col h-full p-2" style="height: 100vh; width: 350px; background-color:#2B2D37;">
+                <div  class="d-flex align-items-center">
+                    <img src="/images/logo.png" alt="NCIP LOGO" class="p-2 h-50">
+                    <div class="d-flex flex-column  text-white">
+                        <span>NCIP R1</span>
+                        <span>Employee Travel Order System</span>
+                    </div>
+                </div>
+                <nav style="margin-top: 100px">
+                    <ul class="list-group" style="line-height: 50px">
+                        <li><a href="#" class="text-decoration-none" style="color:#B2B2B2"><i class='bx bxs-dashboard'></i> Dashboard</a></li>
+                        <li><a href="#" class="text-decoration-none" style="color:#B2B2B2"><i class='bx bxs-file-blank'></i> Request</a></li>
+                        <li><a href="#" class="text-decoration-none" style="color:#B2B2B2"><i class='bx bxs-map' ></i> Travel Orders</a></li>
+                        <li><a href="#" class="text-decoration-none" style="color:#B2B2B2"><i class='bx bxs-user'></i> Employees</a></li>
+                        <li><a href="#" class="text-decoration-none" style="color:#B2B2B2"><i class='bx bxs-user'></i> Officials</a></li>
+                        <li><a href="#" class="text-decoration-none" style="color:#B2B2B2"><i class='bx bx-building' ></i> Divisions</a></li>
+                        <li><a href="#" class="text-decoration-none" style="color:#B2B2B2"><i class='bx bxs-user'></i> Users</a></li>
+                        <li><a href="#" class="text-decoration-none" style="color:#B2B2B2"><i class='bx bxs-cog' ></i> Settings</a></li>
+                    </ul>
+                </nav>
+            </div>
+    </div>
+</body>
+</html>
