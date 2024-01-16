dfbddac2512d98e349bb7d7ae20472de80abb096
patacsilpam <pamelampatacsil@gmail.com>
2024-01-16 22:08:18 +0800

    refactor: added reusable blade template

---
+++ resources/views/includes/superadmin/navbar.blade.php
@@ -0,0 +1,7 @@
+<div class="menu-bar">
+    <i class='bx bx-menu' style="font-size: 24px;"></i>
+</div>
+<div class="profile  d-flex flex-row align-items-center">
+    <img src="/images/logo.png" alt="Profile pic" style="height: 25px;">
+    <span>Juan Dela Cruz</span>
+</div>
---
+++ resources/views/includes/superadmin/sidebar.blade.php
@@ -0,0 +1,66 @@
+<div class="d-flex flex-row align-items-center p-2">
+    <img src="/images/logo.png" alt="NCIP Logo" class="img-logo p-1">
+    <div class="d-none d-md-flex flex-column ">
+        <small>NCIP R1</small>
+        <small>Employee Travel Order System</small>
+       
+    </div>
+</div>
+<ul class="list-group ul-list mt-5">
+    <li class="p-3">
+        <a href="#dashboard" class="text-decoration-none nav-link">
+            <i class='bx bxs-dashboard'></i>
+            <span class="d-none d-md-flex mx-2">Dashboard</span>
+        </a>
+    </li>
+    <li class="p-3">
+        <a href="#request" class="text-decoration-none nav-link"> 
+            <i class='bx bxs-file-blank'></i>
+            <span class="d-none d-md-flex mx-2">Request</span>
+        </a>
+    </li>
+    <li class="p-3">
+        <a href="#to" class="text-decoration-none nav-link"> 
+            <i class='bx bxs-map'></i>
+            <span class="d-none d-md-flex mx-2">Travel Orders</span>
+        </a>
+    </li>
+    <li class="p-3">
+        <a href="#employees" class="text-decoration-none nav-link">
+            <i class='bx bxs-user'></i>
+            <span class=" d-none d-md-flex mx-2">Employees</span>
+        </a>
+    </li>
+    <li class="p-3">
+        <a href="#officials" class="text-decoration-none nav-link">
+            <i class='bx bxs-user'></i> 
+            <span class="d-none d-md-flex mx-2">Officials</span>
+        </a>
+    </li>
+    <li class="p-3">
+        <a href="#division" class="text-decoration-none nav-link">
+            <i class='bx bx-building'></i> 
+            <span class="d-none d-md-flex mx-2">Divisions</span>
+        </a>
+    </li>
+    <li class="p-3">
+        <a href="#users" class="text-decoration-none nav-link"> 
+            <i class='bx bxs-user'></i>
+            <span class="d-none d-md-flex mx-2">Users</span>
+        </a>
+    </li>
+    <li class="p-3" data-bs-toggle="collapse" href="#settingsCollapse" role="button" aria-expanded="false" aria-controls="settingsCollapse">
+        <a href="#settings" class="text-decoration-none nav-link">
+            <i class='bx bxs-cog' ></i>
+            <span class="d-none d-md-flex mx-2">Settings<i class='bx bxs-down-arrow' style="margin-left:150px; margin-top:5px;"></i></span>
+        </a>
+        <!----->
+    </li>
+    <div class="collapse multi-collapse" id="settingsCollapse">
+        <ul class="list-group mt-2 ul-list ">
+            <li class="p-2"><a href="#" class="text-decoration-none text-white">Station</a></li>
+            <li class="p-2"><a href="#" class="text-decoration-none text-white">Position</a></li>
+            <li class="p-2"><a href="#" class="text-decoration-none text-white">Role</a></li>
+        </ul>
+    </div>
+</ul>
