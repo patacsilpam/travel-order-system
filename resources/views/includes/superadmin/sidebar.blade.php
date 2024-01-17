e1567ad5829a95011f53f2724d4bc214fb783dad
patacsilpam <patacsilpamela.bsit@gmil.com>
2024-01-17 17:06:59 +0800

    refactor: navbar and sidebar

--- resources/views/includes/superadmin/sidebar.blade.php
+++ resources/views/includes/superadmin/sidebar.blade.php
@@ -8,49 +8,49 @@
 </div>
 <ul class="list-group ul-list mt-5">
     <li class="p-3">
-        <a href="#dashboard" class="text-decoration-none nav-link">
+        <a href="{{ url('/superadmin/dashboard') }}" class="text-decoration-none nav-link" title="Dashboard">
             <i class='bx bxs-dashboard'></i>
             <span class="d-none d-md-flex mx-2">Dashboard</span>
         </a>
     </li>
     <li class="p-3">
-        <a href="#request" class="text-decoration-none nav-link"> 
+        <a href="#request" class="text-decoration-none nav-link" title="Request"> 
             <i class='bx bxs-file-blank'></i>
             <span class="d-none d-md-flex mx-2">Request</span>
         </a>
     </li>
     <li class="p-3">
-        <a href="#to" class="text-decoration-none nav-link"> 
+        <a href="#to" class="text-decoration-none nav-link" title="Travel Orders"> 
             <i class='bx bxs-map'></i>
             <span class="d-none d-md-flex mx-2">Travel Orders</span>
         </a>
     </li>
     <li class="p-3">
-        <a href="#employees" class="text-decoration-none nav-link">
+        <a href="#employees" class="text-decoration-none nav-link" title="Employees">
             <i class='bx bxs-user'></i>
             <span class=" d-none d-md-flex mx-2">Employees</span>
         </a>
     </li>
     <li class="p-3">
-        <a href="#officials" class="text-decoration-none nav-link">
+        <a href="#officials" class="text-decoration-none nav-link" title="Officials">
             <i class='bx bxs-user'></i> 
             <span class="d-none d-md-flex mx-2">Officials</span>
         </a>
     </li>
     <li class="p-3">
-        <a href="#division" class="text-decoration-none nav-link">
+        <a href="#division" class="text-decoration-none nav-link" title="Divisions">
             <i class='bx bx-building'></i> 
             <span class="d-none d-md-flex mx-2">Divisions</span>
         </a>
     </li>
     <li class="p-3">
-        <a href="#users" class="text-decoration-none nav-link"> 
+        <a href="{{ url('/superadmin/users') }}" class="text-decoration-none nav-link" title="Users"> 
             <i class='bx bxs-user'></i>
             <span class="d-none d-md-flex mx-2">Users</span>
         </a>
     </li>
     <li class="p-3" data-bs-toggle="collapse" href="#settingsCollapse" role="button" aria-expanded="false" aria-controls="settingsCollapse">
-        <a href="#settings" class="text-decoration-none nav-link">
+        <a href="#settings" class="text-decoration-none nav-link" title="Settings">
             <i class='bx bxs-cog' ></i>
             <span class="d-none d-md-flex mx-2">Settings<i class='bx bxs-down-arrow' style="margin-left:150px; margin-top:5px;"></i></span>
         </a>
@@ -58,9 +58,15 @@
     </li>
     <div class="collapse multi-collapse" id="settingsCollapse">
         <ul class="list-group mt-2 ul-list ">
-            <li class="p-2"><a href="#" class="text-decoration-none text-white">Station</a></li>
-            <li class="p-2"><a href="#" class="text-decoration-none text-white">Position</a></li>
-            <li class="p-2"><a href="#" class="text-decoration-none text-white">Role</a></li>
+            <li class="p-2"><a href="#" class="text-decoration-none text-white" title="Station">Station</a></li>
+            <li class="p-2"><a href="#" class="text-decoration-none text-white" title="Position">Position</a></li>
+            <li class="p-2"><a href="#" class="text-decoration-none text-white" title="Role">Role</a></li>
         </ul>
     </div>
 </ul>
+<script>
+        $(document).ready(function () {
+            // If you want to remove the "active" class from all li elements on page load
+            $('li').removeClass('active');
+        });
+    </script>
