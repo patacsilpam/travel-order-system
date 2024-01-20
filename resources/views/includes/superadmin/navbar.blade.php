cd735443a75b9ab166b8874753386ee81cc076c2
patacsilpam <pamelampatacsil@gmail.com>
2024-01-20 22:14:17 +0800

    feat: added dropdown button and links

--- resources/views/includes/superadmin/navbar.blade.php
+++ resources/views/includes/superadmin/navbar.blade.php
@@ -1,7 +1,13 @@
 <div class="menu-bar">
     <i class='bx bx-menu' id="toggleSidebar" style="font-size: 24px;"></i>
 </div>
-<div class="profile d-flex flex-row align-items-center">
+<div class="dropdown">
     <img src="/images/logo.png" alt="Profile pic" style="height: 25px;">
-    <span>Juan Dela Cruz</span>
-</div>
+    <a class="dropdown-toggle text-decoration-none text-dark" href="#" role="button" id="userProfile" data-bs-toggle="dropdown" aria-expanded="false">
+      Juan Dela Cruz
+    </a>
+    <ul class="dropdown-menu" aria-labelledby="userProfile">
+      <li><a class="dropdown-item" href="#">Profile</a></li>
+      <li><a class="dropdown-item" href="#">Logout</a></li>
+    </ul>
+  </div>
