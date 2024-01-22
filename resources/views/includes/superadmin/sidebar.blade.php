d42a7f9fe3db912404b054cffd1514fb0698d228
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-01-22 10:44:14 +0800

    content: added link for role page

--- resources/views/includes/superadmin/sidebar.blade.php
+++ resources/views/includes/superadmin/sidebar.blade.php
@@ -60,7 +60,7 @@
         <ul class="list-group mt-2 ul-list ">
             <li class="p-2"><a href="#" class="text-decoration-none text-white" title="Station">Station</a></li>
             <li class="p-2"><a href="{{ url('/superadmin/settings/position') }}" class="text-decoration-none text-white" title="Position">Position</a></li>
-            <li class="p-2"><a href="#" class="text-decoration-none text-white" title="Role">Role</a></li>
+            <li class="p-2"><a href="{{ url('/superadmin/settings/role') }}" class="text-decoration-none text-white" title="Role">Role</a></li>
         </ul>
     </div>
 </ul>
