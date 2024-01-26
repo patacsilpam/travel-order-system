afa64d4a48bc7730c13873331053303d890568cf
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-01-26 14:47:36 +0800

    routes

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,30 @@ Route::get('/superadmin/dashboard', function () {
     return view('superadmin.dashboard');
 });
 
-Route::get('/superadmin/users', function () {
-    return view('superadmin.users');
+Route::get('/superadmin/travel-requests', function () {
+    return view('superadmin.request');
 });
 
-Route::get('/superadmin/settings/divisions', function () {
-    return view('superadmin.divisions');
+Route::get('/superadmin/travel-orders', function () {
+    return view('superadmin.travel_orders');
+});
+
+
+Route::get('/superadmin/employee', function () {
+    return view('superadmin.employee');
+});
+
+
+Route::get('/superadmin/divisions', function () {
+    return view('superadmin.division');
+});
+
+Route::get('/superadmin/officials', function () {
+    return view('superadmin.official');
+});
+
+Route::get('/superadmin/users', function () {
+    return view('superadmin.users');
 });
 
 Route::get('/superadmin/settings/position', function () {
@@ -32,3 +50,4 @@ Route::get('/superadmin/settings/position', function () {
 Route::get('/superadmin/settings/role', function () {
     return view('superadmin.role');
 });
+
