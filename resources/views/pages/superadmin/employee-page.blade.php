7fc1be7bda3590723f271a883d768d6b3ef0cf1c
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-01-26 14:44:35 +0800

    feat: employee page

---
+++ resources/views/pages/superadmin/employee-page.blade.php
@@ -0,0 +1,31 @@
+<div class="p-2 relative">
+    <div>
+        <h1>Employees</h1>
+    </div><br><br>
+    <table id="example" class="table table-striped" style="width:100%">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Username</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Position</th>
+                <th scope="col">Office</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>juandelacruz</td>
+                <td>Juan</td>
+                <td>Dela Cruz</td>
+                <td></td>
+                <td></td>
+            </tr>
+            
+        </tbody>
+    </table>
+</div>
+<script>
+    new DataTable('#example');
+</script>
