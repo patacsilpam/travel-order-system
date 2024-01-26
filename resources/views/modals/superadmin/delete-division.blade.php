0a51fe8216103d8c6b391cce1e11746c084d91c3
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-01-26 13:32:03 +0800

    feat: added a modal feature for removing a division

---
+++ resources/views/modals/superadmin/delete-division.blade.php
@@ -0,0 +1,22 @@
+<!-- Modal -->
+<div class="modal fade" id="removeDivision" tabindex="-1" role="dialog" aria-labelledby="removeDivision" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="removePosition">Remove Division</h5>
+                <button type="button" class="close border-0" data-dismiss="modal" aria-label="Close"><span class="bg-danger rounded text-white py-1 px-2" aria-hidden="true">&times;</span></button>
+            </div>
+            <form>
+                <div class="modal-body text-center">
+                    <i class='bx bx-error p-3 rounded-circle' style="background: #cd282822; color:#CD2828; font-size:2.5rem"></i>
+                    <h1 class="my-2" style="color:#CD2828; font-size:1.5rem">Are you sure?</h1>
+                    <span class="text-muted my-2 w-10">Do you want delete this record? This action cannot be undone.</span>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-cancel text-muted" data-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn text-white" style="background:#CD2828;">Delete</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
