20b14fe906a8cabcab861cee5adca2c4bc6a27e6
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-02-14 15:32:57 +0800

    feat: user controller

--- app/Http/Controllers/Superadmin/UserController.php
+++ app/Http/Controllers/Superadmin/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\superadmin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -10,5 +11,19 @@ class UserController extends Controller
     //
     public function show(){
         return view('superadmin.users');
+    }
+
+    public function store(Request $request){
+
+        $user = new User;
+        $fname = $request->input('fname');
+        $lname = $request->input('lname');
+        $email = $request->input('email');
+        $position = $request->input('position');
+        $role = $request->input('role');
+        $official = $request->input('official');
+        $password = $request->input('password');
+
+        return redirect()->back()->with('status','Users added successfully');
     }
 }
