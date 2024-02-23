77f3cb0d34cfc1c89751d6c576d3aca6401bd828
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-02-23 11:19:06 +0800

    feat: added stations table

---
+++ database/migrations/2024_02_20_134844_superadmin/2024_02_23_111618_create_stations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stations', function (Blueprint $table) {
+            $table->id();
+            $table->string('department');
+            $table->string('office_name');
+            $table->string('office_address');
+            $table->string('image_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stations');
+    }
+};
