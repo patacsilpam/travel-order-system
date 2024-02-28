49a8a637648ac29c98ab696176abe4ffd53608a3
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-02-28 16:12:12 +0800

    feat: added users data

---
+++ resources/views/pages/superadmin/dashboard.blade.php
@@ -0,0 +1,15 @@
+<div class="p-3">
+    <h1>Dashboard</h1>
+    <div class="users-container bg-white w-50 ">
+        <div>
+            <strong>3</strong>
+            <p>Users</p>
+            <i class='bx bx-user-circle'></i>
+        </div>
+        <hr>
+        <div>
+            <span>View More Info <i class='bx bx-right-arrow-circle'></i></p>
+        </div>
+    </div>
+
+</div>
