e9ba966fd29efed81efa9e726951d18924e2c5a2
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-03-01 15:02:19 +0800

    feat: users statistics container feature

--- resources/views/pages/superadmin/dashboard.blade.php
+++ resources/views/pages/superadmin/dashboard.blade.php
@@ -1,15 +1,20 @@
 <div class="p-3">
-    <h1>Dashboard</h1>
-    <div class="users-container bg-white w-50 ">
-        <div>
-            <strong>3</strong>
-            <p>Users</p>
-            <i class='bx bx-user-circle'></i>
-        </div>
-        <hr>
-        <div>
-            <span>View More Info <i class='bx bx-right-arrow-circle'></i></p>
+    <h1 class="h-dashboard">Dashboard</h1>
+    <div class="mt-5">
+        <div class="bg-white w-25 rounded" style="border: 1px solid #DEE2EA">
+            <div>
+                <div class="d-flex flex-row justify-content-between users-box align-items-center p-2">
+                    <span class="text-count">3</span>
+                    <i class='bx bx-user py-2 bg-circle-icon rounded-circle text-center' ></i>
+                </div>
+                <div>
+                    <span class="text-title p-3">Users</span><hr>
+                </div>
+                <div  class="p-1">
+                    <p style="margin-top:-7px;">More Info <i class='bx bx-right-arrow-circle' style="font-size: 1.2rem"></i></p>
+                </div>
+            </div>
         </div>
     </div>
-
 </div>
+
