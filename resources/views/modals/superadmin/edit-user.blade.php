c14395b74f9a636f9e8b7886d1b16fbe7cd7219f
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-03-08 11:43:07 +0800

    feat: show user information values in each input

--- resources/views/modals/superadmin/edit-user.blade.php
+++ resources/views/modals/superadmin/edit-user.blade.php
@@ -1,5 +1,5 @@
 <!-- Modal -->
-<div class="modal fade" id="editUser" tabindex="-1" role="dialog" aria-labelledby="editUser" aria-hidden="true">
+<div class="modal fade" id="editUser/{{$user->id}}" tabindex="-1" role="dialog" aria-labelledby="editUser" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -11,15 +11,15 @@
                     <strong style="color:#3354F4; font-size:1.1rem">Personal Details</strong>
                     <div class="form-group my-2">
                         <label for="user-firstname">First Name <span class="text-danger">*</span></label>
-                        <input type="text" class="form-control" id="user-firstname" placeholder="Enter First Name" required>
+                        <input type="text" class="form-control" id="user-firstname" value="{{$user->fname}}" placeholder="Enter First Name" required>
                     </div>
                     <div class="form-group my-2">
                         <label for="user-lastname">Last Name <span class="text-danger">*</span></label>
-                        <input type="text" class="form-control" id="user-lastname" placeholder="Enter Last Name" required>
+                        <input type="text" class="form-control" id="user-lastname" value="{{$user->lname}}" placeholder="Enter Last Name" required>
                     </div>
                     <div class="form-group my-2">
                         <label for="user-position">Position <span class="text-danger">*</span></label>
-                        <input type="text" class="form-control" id="user-position" placeholder="Enter Position" required>
+                        <input type="text" class="form-control" id="user-position" value="{{$user->position}}" placeholder="Enter Position" required>
                     </div>
                     <div class="form-group my-2">
                         <label for="user-role">Role <span class="text-danger">*</span></label>
@@ -35,19 +35,26 @@
                         </select>
                     </div>
                     <div class="form-group my-2">
-                        <label for="user-division">Division</label>
+                        <label for="user-division">Office</label>
                         <select class="form-control" id="user-division">
-                            <option selected disabled>Please select</option>
-                            <option value="Empoyee">Empoyee</option>
-                            <option value="Provincial Officer">Provincial Officer</option>
-                            <option value="AFSD Chief">AFSD Chief</option>
-                            <option value="TMSD Chief">TMSD Chief</option>
-                            <option value="Regional Director">Regional Director</option>
+                            <option value="Dingras CSC">Dingras CSC</option>
+                            <option value="Ilocos Norte PO">Ilocos Norte PO</option>
+                            <option value="Banayoyo CSC">Banayoyo CSC</option>
+                            <option value="Banayoyo CSC">Tagudin CSC</option>
+                            <option value="Ilocos Sur PO">Ilocos Sur PO</option>
+                            <option value="Sudipen CSC">Sudipen CSC</option>
+                            <option value="Pugo CSC">Pugo CSC</option>
+                            <option value="Pangasinan PO">Pangasinan PO</option>
+                            <option value="Sison CSC">Sison CSC</option>
+                            <option value="Urdaneta CSC">Urdaneta CSC</option>
+                            <option value="Regional Office - AFSD">Regional Office - AFSD</option>
+                            <option value="Regional Office - TMSD">Regional Office - TMSD</option>
+                            <option value="Regional Office - ORD">Regional Office - ORD</option>
                         </select>
                     </div>
                     <div class="form-group my-2">
                         <label for="user-email-address">Email address <span class="text-danger">*</span></label>
-                        <input type="email" class="form-control" id="user-email-address" placeholder="Enter email address" required>
+                        <input type="email" class="form-control" id="user-email-address"  value="{{$user->email}}" placeholder="Enter email address" required>
                     </div>
                     <div class="form-group my-2">
                         <label for="user-password">Password <span class="text-danger">*</span></label>
